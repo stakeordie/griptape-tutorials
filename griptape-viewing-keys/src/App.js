070c99a6b33f9849ac8bd8a44e2b02e4126b72f4
VPringle <valeria.zuniga>
2022-01-11 09:19:42 -0800

    feat: add griptape-viewing-keys

---
+++ griptape-viewing-keys/src/App.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from "react";
+import {
+  bootstrap,
+  viewingKeyManager,
+  onAccountAvailable,
+  coinConvert
+} from '@stakeordie/griptape.js';
+import { sscrt } from './contracts/sscrt';
+
+function App() {
+
+  var [loading, setLoading] = useState(false);
+  var [coins, setCoins] = useState('');
+  var [viewingKey, setViewingKey] = useState('');
+
+  useEffect(() => {
+    onAccountAvailable(() => {
+      const key = viewingKeyManager.get(sscrt.at);
+      if (key) {
+        setViewingKey(key);
+      }
+    })
+  }, []);
+
+  const connect = async () => {
+    await bootstrap();
+  }
+
+  const getBalance = async () => {
+    const key = viewingKeyManager.get(sscrt.at);
+
+    if (!key) return;
+
+    const amount = await sscrt.getBalance();
+    const balance = coinConvert(amount.balance.amount, 6, 'human');
+
+    setCoins(balance);
+  }
+
+  const createViewingKey = async () => {
+
+    setLoading(true);
+    try {
+      const result = await sscrt.createViewingKey();
+
+      if (result.isEmpty()) return;
+
+      const { create_viewing_key: { key } } = result.parse();
+      viewingKeyManager.add(sscrt, key);
+      setViewingKey(key);
+
+    } catch (e) {
+      // ignore for now
+    } finally {
+
+      setLoading(false);
+    }
+
+  }
+
+  return (
+    <>
+      <h1>Hello, Griptape!</h1>
+      <p>Your viewing key is: {viewingKey}</p>
+      <p>Your balance is: {coins}</p>
+      <button onClick={() => { connect(); }}>Connect</button>
+      <button onClick={() => { createViewingKey() }}>{loading ? 'Loading...' : 'Create Viewing Key'}</button>
+      <button onClick={() => { getBalance() }}>Get balance</button>
+    </>
+  );
+}
+
+export default App;
