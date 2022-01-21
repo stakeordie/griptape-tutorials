ea4f385eb5780f20f025179f2d3c3ece17a92fe7
David-Sod <david@stakeordie.com>
2022-01-21 13:39:30 -0800

    feat: Add hello-mint in vuejs

---
+++ vue/hello-mint/src/App.vue
@@ -0,0 +1,113 @@
+<template>
+  <div>
+    <h1>Hello, Griptape!</h1>
+    <button @click="connect">Connect</button>
+    <button @click="mint">{{loadingMint ? 'Loading...' : 'Mint'}}</button>
+    <br />
+    <br />
+    <button @click="getTokens">
+      {{loadingTokens ? 'Loading...' : 'Get Tokens'}}
+    </button>
+    <button @click="createViewingKey">
+      {{loading ? 'Loading...' : 'Create Viewing Key'}}
+    </button>
+    <br />
+    <div v-bind:key="item.name" v-for="item in tokens">
+      <div>
+        <img v-if="item.image" :src="item.image" alt="[image]" />
+        <img
+          v-else
+          src="https://i.picsum.photos/id/551/200/300.jpg?hmac=pXJCWIikY_BiqwhtawBb8x1jxclDny0522ZprZVTJiU"
+          alt="[image]"
+        />
+      </div>
+      <div>{{item.name}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  viewingKeyManager,
+  bootstrap
+} from '@stakeordie/griptape.js';
+import { minting } from './contracts/minting';
+
+export default {
+  data() {
+    return {
+      loading: false,
+      loadingMint:false,
+      loadingTokens:false,
+      tokens:[]
+    }
+  },
+  methods: {
+    async createViewingKey  () {
+      this.loading = true;
+      try {
+        const result = await minting.createViewingKey();
+
+        if (result.isEmpty()) return;
+
+        const { viewing_key: { key } } = result.parse();
+        viewingKeyManager.add(minting, key);
+      } catch (e) {
+        console.error(e)
+      } finally {
+        this.loading = false;
+      }
+    },
+    async connect() {
+      await bootstrap();
+    },
+    async mint(){
+      var date = Date.now();
+      const data = {
+        name: `Example ${date}`,
+        description: "test",
+        image: 'https://i.picsum.photos/id/586/200/300.jpg?hmac=Ugf94OPRVzdbHxLu5sunf4PTa53u3gDVzdsh5jFCwQE'
+      }
+      this.loadingMint = true;
+      try {
+        const mint = await minting.mintNft(data);
+      } catch (e) {
+        // ignore for now
+      } finally {
+        this.loadingMint = false;
+      }
+  },
+  async getTokens(){
+    this.loadingTokens = true;
+    try {
+      //Get list of tokens' id owned
+      // Exam. ["4","65","87"]
+      const tokens = await minting.getTokens();
+      const token_list = tokens.token_list.tokens;
+      //Get details of each token
+      await this.getNftDetail(token_list);
+    } catch (e) {
+      console.error(e)
+    } finally {
+      this.loadingTokens = false;
+    }
+  },
+  async getNftDetail(token_list) {
+    const promises = token_list.map(token => {
+      //Query each token 
+      return minting.getNftDossier(token);
+    });
+
+    const result = await Promise.allSettled(promises);
+    this.tokens = result
+      .filter(item => item.status === 'fulfilled')
+      .map(item => item.value.nft_dossier)
+      .map(({ public_metadata: { extension } }) => ({
+        name: extension.name,
+        description: extension.description,
+        image: extension.image
+      }));
+    }
+  }
+}
+</script>
---
+++ vue/hello-mint/src/contracts/minting.js
@@ -0,0 +1,43 @@
+import {
+  createContract,
+  snip721Def,
+  extendContract
+} from '@stakeordie/griptape.js';
+
+const def = {
+  messages:{
+    mintNft(ctx, {name, description, image}){
+      const handleMsg = {
+        mint_nft: {
+          owner:ctx.address,
+          public_metadata: {
+            extension: {
+              name, 
+              description,
+              image
+            }
+          }
+        }
+      };
+      return {handleMsg};
+    }
+  },
+  queries:{
+    getTokens({address, key}){
+      return {tokens:{
+        viewer: address,
+        owner: address,
+        viewing_key: key
+      }}
+    }
+  }
+};
+
+const definition = extendContract(snip721Def, def);
+
+export const minting = createContract({
+  id: 'nft',
+  at: 'secret1kmuezv2h428mepwq80utphggfccvul9dhqwd95',
+  definition: definition
+});
+  
