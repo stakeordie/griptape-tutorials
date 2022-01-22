12c6c233fecff8fd3cfb75a8475cbabb1874cb4c
Luis Espinoza <luis@stakeordie.com>
2022-01-21 17:59:17 -0800

    refactor: Complete refactor

--- react/hello-transactions/src/App.js
+++ react/hello-transactions/src/App.js
@@ -1,56 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   bootstrap,
-  coinConvert,
+  onAccountAvailable,
+  onViewingKeyCreated,
   viewingKeyManager,
+  coinConvert
 } from "@stakeordie/griptape.js";
 import { sscrt } from "./contracts/sscrt";
 
 function App() {
-  const [isQueryLoading, setQueryLoading] = useState(false);
-  const [isMessageLoading, setMessageLoading] = useState(false);
-  const [tokens, setTokens] = useState("");
-  const [amount, setAmount] = useState("");
+  const [isConnected, setIsConnected] = useState(false);
+  const [isMessageLoading, setMessageLoading] =
+    useState(false);
+  const [isQueryLoading, setQueryLoading] =
+    useState(false);
+  const [balance, setBalance] = useState("");
   const [address, setAddress] = useState("");
+  const [amount, setAmount] = useState(0);
 
-  const sendTokens = async () => {
-    setMessageLoading(true);
+  useEffect(() => {
+    onAccountAvailable(() => {
+      setIsConnected(true);
+      hasViewingKey();
+    });
 
-    try {
-      const theAmount = coinConvert(amount, 6, "machine");
-      await sscrt.send(address, theAmount);
+    onViewingKeyCreated(() => {
+      hasViewingKey();
+    });
+  }, []);
 
-      setAmount("");
-      setAddress("");
-    } finally {
-      setMessageLoading(false);
-    }
-  }
-
-  const getBalance = async () => {
-    const key = viewingKeyManager.get(sscrt.at);
-
-    if (!key) return;
-
-    setQueryLoading(true);
-
-    try {
-      const { balance: { amount } } = await sscrt.getBalance();
-      const balance = coinConvert(amount, 6, "human");
-      setTokens(balance);
-    } finally {
-      setQueryLoading(false);
-    }
-  }
-
-  const createViewingKey = async () => {
+  async function createViewingKey() {
     setMessageLoading(true);
 
     try {
       const result = await sscrt.createViewingKey();
       if (result.isEmpty()) return;
-      const { create_viewing_key: { key } } = result.parse();
+      const { create_viewing_key: { key } } =
+        result.parse();
       viewingKeyManager.add(sscrt, key);
+    } finally {
+      setMessageLoading(false);
+    }
+  }
+  
+  function hasViewingKey() {
+    const key = viewingKeyManager.get(sscrt.at);
+    return typeof key !== "undefined";
+  }
+
+  async function getBalance() {
+    if (!hasViewingKey()) return;
+
+    setQueryLoading(true);
+    try {
+      const { balance: { amount: result } } =
+        await sscrt.getBalance();
+      const amount = coinConvert(result, 6, "human");
+      setBalance(amount);
+    } finally {
+      setQueryLoading(false);
+    }
+  }
+
+  async function sendTokens(e) {
+    e.preventDefault();
+
+    if (!address || !amount) return;
+
+    setMessageLoading(true);
+
+    try {
+      const theAmount = coinConvert(amount, 6, "machine");
+      await sscrt.send(address, theAmount);
+      setAddress("");
+      setAmount(0);
     } finally {
       setMessageLoading(false);
     }
@@ -58,29 +81,51 @@ function App() {
 
   return (
     <>
-      <h1>Hello, Griptape!</h1>
-      <p>Your balance is: {tokens}</p>
-      <button onClick={() => bootstrap()}>Connect</button>
-      <button onClick={() => createViewingKey()} disabled={isMessageLoading}>Create Viewing Key</button>
-      <button onClick={() => getBalance()} disabled={isQueryLoading || isMessageLoading}>Update Balance</button>
-
-      <br/>
-
-      <input
-        placeholder="Address to send to"
-        onChange={(e) => setAddress(e.target.value)}
-        value={address}/>
-
-      <br/>
-
-      <input
-        placeholder="Amount to send"
-        onChange={(e) => setAmount(e.target.value)}
-        value={amount}/>
-
-      <br/>
-
-      <button onClick={() => sendTokens()} disabled={isMessageLoading}>Send tokens</button>
+      <h1>Hello, Transactions!</h1>
+      <p>Is connected? { isConnected ? "Yes": "No" }</p>
+      <p>
+        Has viewing key? { hasViewingKey() ? "Yes" : "No" }
+      </p>
+      <p>SNIP-20 Token Balance: { balance }</p>
+      <button
+        onClick={() => bootstrap()}
+        disabled={isConnected}
+      >
+        Connect
+      </button>
+      <button
+        onClick={() => createViewingKey()}
+        disabled={
+            isMessageLoading
+          || hasViewingKey()
+          || !isConnected
+        }
+      >
+        Create Viewing Key
+      </button>
+      <button
+        onClick={() => getBalance()}
+        disabled={isQueryLoading}
+      >
+        Get Balance
+      </button>
+      <form onSubmit={sendTokens}>
+        <input
+          type="text"
+          placeholder="Address to send to"
+          onChange={(e) => setAddress(e.target.value)}
+          value={address}
+        />
+        <input
+          type="number"
+          placeholder="Amount to send"
+          onChange={(e) => setAmount(e.target.value)}
+          value={amount}
+        />
+        <button disabled={isMessageLoading}>
+          Send tokens
+        </button>
+      </form>
     </>
   );
 }
--- react/hello-transactions/src/index.js
+++ react/hello-transactions/src/index.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import {
   gripApp,
   getKeplrAccountProvider
-} from '@stakeordie/griptape.js';
+} from "@stakeordie/griptape.js";
 
-const restUrl = 'https://api.pulsar.griptapejs.com';
+const restUrl = "https://api.pulsar.griptapejs.com";
 const provider = getKeplrAccountProvider();
 function runApp() {
-
-    ReactDOM.render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-      document.getElementById('root')
-    );
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
 }
 
 gripApp(restUrl, provider, runApp);
