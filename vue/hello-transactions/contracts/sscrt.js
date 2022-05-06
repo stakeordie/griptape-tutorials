c609671b00b35a030d45108c2670d84bee50d319
Valeria Pringle <valeria.zuniga@lkmx.io>
2022-05-06 09:28:27 -0700

    feat: extend contract in hello transactions

--- vue/hello-transactions/contracts/sscrt.js
+++ vue/hello-transactions/contracts/sscrt.js
@@ -1,10 +1,30 @@
 import {
   createContractClient,
-  snip20Def
+  snip20Def,
+  extendContract
 } from "@stakeordie/griptape.js";
+
+const contractDef = {
+  messages: {
+    send({ padding }, recipient, amount, msg) {
+      const handleMsg = {
+        send: { recipient, amount, msg, padding },
+      };
+      return { handleMsg, fees:36000 };
+    },
+    createViewingKey({ padding, entropy }) {
+      const handleMsg = {
+        create_viewing_key: { entropy, padding },
+      };
+      return { handleMsg, fees:36000 };
+    },
+  },
+};
+
+const extendedDef = extendContract(snip20Def, contractDef);
 
 export const sscrt = createContractClient({
   id: "sscrt",
   at: "secret18vd8fpwxzck93qlwghaj6arh4p7c5n8978vsyg",
-  definition: snip20Def
+  definition: extendedDef,
 });
