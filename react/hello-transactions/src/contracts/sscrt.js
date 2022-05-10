aa222784f80c9dcda3b1ce67d26067b525467fa0
Valeria Pringle <valeria.zuniga@lkmx.io>
2022-05-10 11:46:20 -0700

    feat: update hello-transactions react

--- react/hello-transactions/src/contracts/sscrt.js
+++ react/hello-transactions/src/contracts/sscrt.js
@@ -1,10 +1,31 @@
 import {
   createContractClient,
-  snip20Def
+  snip20Def,
+  extendContract
 } from "@stakeordie/griptape.js";
+
+const contractDef = {
+  messages: {
+    send({ padding }, recipient, amount, msg) {
+      const handleMsg = {
+        send: { recipient, amount, msg, padding },
+      };
+      return { handleMsg, fees:100000 };
+    },
+    createViewingKey({ padding, entropy }) {
+      const handleMsg = {
+        create_viewing_key: { entropy, padding },
+      };
+      return { handleMsg, fees:36000 };
+    },
+  },
+};
+
+const extendedDef = extendContract(snip20Def, contractDef);
 
 export const sscrt = createContractClient({
   id: "sscrt",
   at: "secret18vd8fpwxzck93qlwghaj6arh4p7c5n8978vsyg",
-  definition: snip20Def
+  codeHash: "9587d60b8e6b078ace12014ceeee089530b9fabcd76535d93666a6c127ad8813",
+  definition: extendedDef,
 });
--- react/hello-transactions/src/index.js
+++ react/hello-transactions/src/index.js
@@ -1,20 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-import {
-  gripApp,
-  getKeplrAccountProvider
-} from "@stakeordie/griptape.js";
+import { gripApp, getKeplrAccountProvider } from "@stakeordie/griptape.js";
 
-const restUrl = "https://api.pulsar.griptapejs.com";
+const restUrl = "http://rpc.pulsar.griptapejs.com:9091";
 const provider = getKeplrAccountProvider();
 function runApp() {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
+  const container = document.getElementById("root");
+  const root = createRoot(container);
+  root.render(<App />);
 }
 
 gripApp(restUrl, provider, runApp);
