36132afde7ad2cbeb476da560ca1259c22ddbaf6
Valeria Pringle <valeria.zuniga@lkmx.io>
2022-05-10 10:41:01 -0700

    feat: update hello-permits react

--- react/hello-permits/src/contracts/sscrt.js
+++ react/hello-permits/src/contracts/sscrt.js
@@ -1,22 +1,22 @@
 import {
   createContractClient,
   snip20Def,
-  extendContract
-} from '@stakeordie/griptape.js';
+  extendContract,
+} from "@stakeordie/griptape.js";
 
 const sscrt_permit = {
   queries: {
     getBalance({ permit }) {
-      const query = { balance: {} }
+      const query = { balance: {} };
       return {
-        with_permit: { query, permit }
-      }
-    }
-  }
-}
-
+        with_permit: { query, permit },
+      };
+    },
+  },
+};
 export const sscrt = createContractClient({
-  id: 'sscrt',
-  at: 'secret18vd8fpwxzck93qlwghaj6arh4p7c5n8978vsyg',
-  definition: extendContract(snip20Def, sscrt_permit)
+  id: "sscrt",
+  at: "secret18vd8fpwxzck93qlwghaj6arh4p7c5n8978vsyg",
+  codeHash: "9587d60b8e6b078ace12014ceeee089530b9fabcd76535d93666a6c127ad8813",
+  definition: extendContract(snip20Def, sscrt_permit),
 });
--- react/hello-permits/src/index.js
+++ react/hello-permits/src/index.js
@@ -1,21 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {
-  gripApp,
-  getKeplrAccountProvider
-} from '@stakeordie/griptape.js';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { gripApp, getKeplrAccountProvider } from "@stakeordie/griptape.js";
 
-const restUrl = 'https://api.pulsar.griptapejs.com';
+const restUrl = "http://rpc.pulsar.griptapejs.com:9091";
 const provider = getKeplrAccountProvider();
 function runApp() {
-
-    ReactDOM.render(
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>,
-      document.getElementById('root')
-    );
+  const container = document.getElementById("root");
+  const root = createRoot(container);
+  root.render(<App />);
 }
 
 gripApp(restUrl, provider, runApp);
